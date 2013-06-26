96fe441e8f1a0919c0ffa1bb18b7025bab5bcfc9
Matt Delves <matt@reformedsoftware.com>
2013-06-26 21:42:52 +1000

    [UPDATE] delegate latitude and longitude to latlon

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ActiveRecord::Base
   set_rgeo_factory_for_column(:latlon,
                               RGeo::Geographic.spherical_factory(:srid => 4326))
+
+  delegate :latitude, :to => :latlon
+  delegate :longitude, :to => :latlon
 end
